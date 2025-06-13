ec7c793462d46ea116ad39b8e00f72cbb656cc13
Chantha makara <chantha.makara@institute.pse.ngo>
2025-06-13 08:17:40 +0700

    fix header and add logout button

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,4 @@
 
-<!-- <script setup>
-import Header from './components/header.vue';
-import Dashboard from './components/dashboard.vue';
-</script>
-
-
-<template>
-  <Header />
-  <Dashboard />
-</template> -->
-
 
 <template>
   <div id="app">
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,19 +1,28 @@
 <template>
-      <header class="bg-[#F8F8E1]">
+    <header class="bg-[#F8F8E1]">
     <div class="flex justify-between px-4">
+
       <div class="w-40 h-20 ">
         <img class="w-full h-full" :src="logo" alt="Image">
       </div>
-      <div class="w-60 h-20 flex justify-between">
+
+      <div class="w-90 h-20 flex justify-between">
         <div class=" w-40 ms-4 mt-2">
           <h1 class="text-lg font-bold">Chantha Makara</h1>
           <p class="text-sm ml-0 text-gray-500"> Librarian</p>
         </div>
-
         <div class=" rounded-full mt-2">
           <img class="w-20 h-[95%] shadow-xl rounded-full" :src="makara" alt="Image">
         </div>
+        <div>
+          <router-link to="/Login" class="rounded-xl text-white ">
+              <div class="rounded-xl px-4 py-2 ms-4 bg-pink-500 mt-5 hover:bg-pink-800 hover:shadow-2xl">
+                <p class="font-bold">Logout</p>
+              </div>
+          </router-link>
+        </div>
       </div>
+
     </div>
   </header>
 </template>
