f612a777dd3f650b01a4ec416e5acab8b09a1d3a
Chantha makara <chantha.makara@institute.pse.ngo>
2025-06-16 10:34:07 +0700

    done add new book

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,8 +1,9 @@
 <script setup>
-
+import newBook from '../pages/Add-book.vue'
 </script>
 
 <template>
   <div>this is login page</div>
   <router-link to="/home">login</router-link>
+  <newBook />
 </template>
