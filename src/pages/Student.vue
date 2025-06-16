8dca816ab96a7b83cd103f806493ba82c4029ea3
sreypheak <phorn.sreypheak@institute.pse.ngo>
2025-06-16 10:27:20 +0700

    add student about button

--- src/pages/Student.vue
+++ src/pages/Student.vue
@@ -20,14 +20,20 @@ const students = [
 
 <template>
   <layoutdashboard />
-  <div class="bg-[#F8F8E1] rounded-2xl shadow -mt-120 ms-70">
+  <div class="bg-[#F8F8E1] rounded-2xl shadow -mt-115 ms-70 p-3">
     <div class="w-[63rem] h-auto">
-      <div class="w-[90%] mx-auto mt-5 py-5">
+      <div class="w-[90%] mx-auto py-5">
         <h1 class="text-4xl font-bold">Welcome to Students page</h1>
         <p class="text-2xl">jun 11 , 2025 | Wednesday, 8:00 am</p>
       </div>
     </div>
-    <table class="divide-y divide-gray-200 w-[90%] mx-auto mt-5">
+    <div
+      class="w-[20%] ml-[760px] rounded-sm px-4 py-2 ms-4 bg-pink-500 hover:bg-pink-800 hover:shadow-2xl"
+    >
+      <p class="font-bold text-center text-white">+ Add New Student</p>
+    </div>
+    <router-link to="" class="rounded-xl text-white"> </router-link>
+    <table class="divide-gray-200 w-[90%] mx-auto mt-5">
       <thead class="bg-white">
         <tr>
           <th class="px-6 py-3 text-left text-sm font-semibold text-gray-900">
@@ -55,10 +61,11 @@ const students = [
         </tr>
       </tbody>
     </table>
-    <div class="text-right mt-4 w-[90%] mx-auto">
+
+    <div class="text-right w-[90%] mx-auto bg-white">
       <a
         href="#"
-        class="text-pink-400 hover:text-pink-600 text-sm font-semibold"
+        class="text-pink-400 hover:text-pink-600 text-sm font-semibold mr-2"
         >See All</a
       >
     </div>
